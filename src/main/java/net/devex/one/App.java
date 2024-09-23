3a1ed37125c4397facc790cc3715858469e3400d
TO DUY THIEN LUONG <toduythienluong@gmail.com>
2024-09-23 11:17:14 +0700

    modules

---
+++ src/main/java/net/devex/one/App.java
@@ -0,0 +1,13 @@
+package net.devex.one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
